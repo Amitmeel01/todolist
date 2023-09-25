c4617655c02b45e0a4b37b9351a102d7aee32ed8
Amitmeel01 <meelamit354@gmail.com>
2023-09-25 10:32:13 +0530

    todolist

---
+++ app.js
@@ -0,0 +1,24 @@
+let inp=document.querySelector("input");
+let ul=document.querySelector("ul");
+let btn=document.querySelector("button");
+btn.addEventListener("click",function(){
+    let item=document.createElement("li");
+    item.innerText=inp.value;
+
+    let delbtn=document.createElement("button");
+    delbtn.innerText="Delete";
+    delbtn.classList.add("delete");
+    item.appendChild(delbtn);
+    ul.appendChild(item);
+    inp.value="";
+    
+});
+
+ul.addEventListener("click",function(event){
+    if(event.target.nodeName=="BUTTON"){
+        let listitem=event.target.parentElement;
+        listitem.remove();
+        console.log("deleted");
+    }
+
+});
